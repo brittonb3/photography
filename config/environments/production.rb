d2470a62f6d493c74730e71ffb81714ab6a1fdb6
Britton Baumann <brittonbaumann@gmail.com>
2017-12-21 10:20:25 -0600

    able to recieve emails through heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   config.serve_static_assets = true
   config.assets.compile = true
+
+  config.action_mailer.default_url_options = { host: 'https://ancient-sea-72846.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
