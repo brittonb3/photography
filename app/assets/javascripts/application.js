048d3e115379f012fc0e8631bca5f7597b1c7c31
Britton Baumann <brittonbaumann@gmail.com>
2018-08-04 20:30:23 -0500

    finished last update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -157,4 +157,55 @@ for(let i = 0; i < portLinks.length; i++) {
 
 
 
+////////////// Prevent right click ////////////
+let copyAlert = document.getElementById('copy-alert');
+let body = document.getElementsByTagName('body')[0];
+
+window.onload = function() {
+    document.addEventListener("contextmenu", function(e){
+      e.preventDefault();
+      copyAlert.style.visibility = 'visible';
+    }, false);
+    document.addEventListener("keydown", function(e) {
+    //document.onkeydown = function(e) {
+      // "I" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        disabledEvent(e);
+      }
+      // "J" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        disabledEvent(e);
+      }
+      // "S" key + macOS
+      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        disabledEvent(e);
+      }
+      // "U" key
+      if (e.ctrlKey && e.keyCode == 85) {
+        disabledEvent(e);
+      }
+      // "F12" key
+      if (event.keyCode == 123) {
+        disabledEvent(e);
+      }
+    }, false);
+    function disabledEvent(e){
+      if (e.stopPropagation){
+        e.stopPropagation();
+      } else if (window.event){
+        window.event.cancelBubble = true;
+      }
+      e.preventDefault();
+      return false;
+    }
+
+  };
+
+
+      body.addEventListener('click', function() {
+        if(copyAlert.style.visibility === 'visible') {
+          copyAlert.style.visibility = 'hidden';
+        }
+      })
+
 });
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form",
-      :to => "brittonbaumann@gmail.com",
+      :to => "brittonbaumann@greatbrittonphotography.com",
       :from => %("#{name}" <#{email}>)
     }
   end
